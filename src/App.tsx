fc1a1d4f29b586c51487b54eeecb3070efe16cde
gabriel.brito <gabriefranccelino@gmail.com>
2021-02-02 08:41:12 -0300

    feat: route architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <h1>App...</h1>
-    </div>
-  );
-}
+import Routes from "./routes";
 
-export default App;
+const APP: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
+
+export default APP;
